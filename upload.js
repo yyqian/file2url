e28fe3bf1a18abad9dd178c9555a74c9b9dab7fd
Yinyin Qian <yyqian@live.com>
2016-04-17 23:12:14 +0800

    ok

--- upload.js
+++ upload.js
@@ -1,3 +1,31 @@
 'use strict';
-const http = require('http');
+const qiniu = require("qiniu");
 const config = require('./config.js');
+
+qiniu.conf.ACCESS_KEY = config.ACCESS_KEY;
+qiniu.conf.SECRET_KEY = config.SECRET_KEY;
+//const bucket = config.bucket;
+const bucket = 'test';
+const domain = config.domain;
+
+const getUptoken = bucket => {
+  const putPolicy = new qiniu.rs.PutPolicy(bucket);
+  return putPolicy.token();
+};
+
+//构造上传函数
+const uploadFile = (bucket, localFile) => {
+  const uptoken = getUptoken(bucket);
+  const extra = new qiniu.io.PutExtra();
+  qiniu.io.putFileWithoutKey(uptoken, localFile, extra, (err, ret) => {
+    if (!err) {
+      console.log(ret);
+    } else {
+      console.log(err);
+    }
+  });
+};
+
+// Run
+const filePath = './README.md';
+uploadFile(bucket, filePath);
