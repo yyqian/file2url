8c76b00eac6c3eb9cead93d715488f683e9c78d8
Yinyin Qian <yyqian@live.com>
2016-04-18 13:28:01 +0800

    complete

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # qiniu-upload
+
+主要用于 Markdown 写作时插入图片和文件超链接的需求.
+
+在命令行中指定多个要上传的文件或文件夹, 该应用将自动上传文件到七牛云, 并且返回 Markdown 格式的图片或文件的链接.
+
+使用方法:
+
+- 复制 config-sample.js 到 config.js
+- 配置 config.js, 主要是七牛云账户相关的信息
+- 运行命令 `node upload.js args...`
+
+`args` 可以是多个文件名或者文件夹名称. 如果是文件夹名称, 会上传该文件夹下所有文件（包括 . 开头的隐藏文件, 但不包括子文件夹）
+
+返回格式如下:
+
+```
+[desktop.ini](http://cdn.yyqian.com/201604181255-FjDO2SfiO1hHJc8WNROUF1ptKpV3?attname=desktop.ini)
+[upload.js](http://cdn.yyqian.com/201604181255-FtPw_MbFHH8amIj5TVyONSkKy9KB?attname=upload.js)
+[upload-history.log](http://cdn.yyqian.com/201604181255-FrNtXI0y-X_BFJ7LVsDhD5YGdD2Q?attname=upload-history.log)
+![ss.jpg](http://cdn.yyqian.com/201604181255-FuyxlbTOMYTV4It7CAKqbB8Typas?imageView2/2/w/800/h/600)
+```
+
+也可以根据需要使用 bash script 将输出复制到粘贴板, 见 upload.sh 示例脚本.
--- upload.js
+++ upload.js
@@ -1,31 +1,61 @@
 'use strict';
-const qiniu = require("qiniu");
+const path = require('path');
+const fs = require('fs');
+const qiniu = require('qiniu');
 const config = require('./config.js');
 
 qiniu.conf.ACCESS_KEY = config.ACCESS_KEY;
 qiniu.conf.SECRET_KEY = config.SECRET_KEY;
-//const bucket = config.bucket;
-const bucket = 'test';
-const domain = config.domain;
 
 const getUptoken = bucket => {
-  const putPolicy = new qiniu.rs.PutPolicy(bucket);
+  const putPolicyObj = {
+    scope: bucket,
+    returnBody: '{"key":$(key),"mimeType":$(mimeType)}',
+    saveKey: '${year}${mon}${day}${hour}${min}-$(etag)'
+  };
+  const putPolicy = new qiniu.rs.PutPolicy2(putPolicyObj);
   return putPolicy.token();
 };
 
 //构造上传函数
-const uploadFile = (bucket, localFile) => {
+const uploadFile = (bucket, domain, localFile) => {
   const uptoken = getUptoken(bucket);
   const extra = new qiniu.io.PutExtra();
+  const attname = path.basename(localFile);
   qiniu.io.putFileWithoutKey(uptoken, localFile, extra, (err, ret) => {
     if (!err) {
-      console.log(ret);
+      let url = "http://" + domain + "/" + ret.key;
+      let markdown;
+      if (ret.mimeType != null && ret.mimeType.startsWith("image")) {
+        url += "?imageView2/2/w/800/h/600";
+        markdown = getMarkdownImage(url, attname);
+      } else {
+        url = url + "?attname=" + attname;
+        markdown = getMarkdownLink(url, attname);
+      }
+      console.log(markdown);
     } else {
       console.log(err);
     }
   });
 };
 
+const getMarkdownLink = (link, text) => '[' + text + '](' + link + ')';
+const getMarkdownImage = (link, text) => '![' + text + '](' + link + ')';
+const dig = (pathName, depth, process) => {
+  if (fs.statSync(pathName).isDirectory() && depth > 0) {
+    fs.readdirSync(pathName).forEach(val => {
+      dig(pathName + '/' + val, depth - 1, process);
+    });
+  } else if (fs.statSync(pathName).isFile()) {
+    process(pathName);
+  }
+};
+
 // Run
-const filePath = './README.md';
-uploadFile(bucket, filePath);
+const args = process.argv.slice(2);
+args.forEach(pathName => {
+  dig(path.resolve(pathName), 1, val => {
+    uploadFile(config.bucket, config.domain, val);
+  });
+});
